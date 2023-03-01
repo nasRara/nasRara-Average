ebeca60f1508ff077ae1b3a8df46378495863218
nasRara <pastasaqajan@icloud.com>
2023-03-01 22:14:17 +0000

    average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,13 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int total=0;
+        for(int i= 0; i< nums.length; i++){
+            total += + nums[i];
+        }
+        double average = (double) total/ nums.length; 
+
+
+        return average;
     }
 }
